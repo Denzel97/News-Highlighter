f1067bdaf83edf234ecbf3ae062cf82438387962
Denzel97 <denzelwash97@gmail.com>
2019-02-06 17:25:21 +0300

    add app/models/article.py

--- app/models/article.py
+++ app/models/article.py
@@ -3,10 +3,10 @@ class Article:
     Article class to define Article Objects
     '''
 
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
-        self.id =id
-        self.title = title
-        self.overview = overview
-        self.poster = 'https://image.tmdb.org/t/p/w500/'+ poster
-        self.vote_average = vote_average
-        self.vote_count = vote_count
+    def __init__(self,author,title,description,url,urlToImage,publishedAt):
+        self.author =author
+        self.title =title
+        self.description =description
+        self.url =url
+        self.urlToImage =urlToImage
+        self.publishedAt =publishedAt
