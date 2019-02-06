dc57da17bbb2419881e586e4dd36eed7e16ec0e6
Denzel97 <denzelwash97@gmail.com>
2019-02-06 17:26:21 +0300

    modified: templates/

--- app/views.py
+++ app/views.py
@@ -3,13 +3,13 @@ from app import app
 from .request import get_articles
 
 # Views
-@app.route('/article/<int:article_id>')
-def arricle(article_id):
-
-    '''
-    View article page function that returns the article details page and its data
-    '''
-    return render_template('article.html',id = article_id)
+# @app.route('/article/<int:article_id>')
+# def arricle(article_id):
+#
+#     '''
+#     View article page function that returns the article details page and its data
+#     '''
+#     return render_template('article.html',id = article_id)
 
 @app.route('/')
 def index():
@@ -19,8 +19,7 @@ def index():
     '''
 
     # Getting popular article
-    popular_articles = get_articles('popular')
-    upcoming_article = get_articles('upcoming')
-    now_showing_article = get_articles('now_playing')
+    general_articles = get_articles('article')
+
     title = 'Home - Welcome to The best Article Review Website Online'
-    return render_template('index.html', title = title, popular = popular_articles, upcoming = upcoming_article, now_showing = now_showing_movie )
+    return render_template('index.html', title = title, article = general_articles)
