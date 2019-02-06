90f5db6cd09dba14d4766781159cd8bd4686211d
Denzel97 <denzelwash97@gmail.com>
2019-02-06 12:37:32 +0300

    add config.py

---
+++ app/config.py
@@ -0,0 +1,27 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
